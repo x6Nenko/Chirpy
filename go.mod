189348320abedbc18de00cdb01e80b69c40d351d
Max I. <clickkava@gmail.com>
2025-11-12 21:45:01 +0200

    Init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/x6Nenko/Chirpy
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Creating a new ServeMux
+	ServeMux := http.NewServeMux()
+
+	// Creating a server struct
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: ServeMux,
+	}
+
+	// Start the server
+	log.Printf("Serving on port: 8080\n")
+	log.Fatal(server.ListenAndServe())
+}
